321e70aba1f6193b680a920da45cf212afa827e2
AlteonRea <alteonreastra@gmail.com>
2024-10-14 22:10:45 +0700

    idk

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,29 +20,29 @@
         <h3 class="font-poppins my-4 text-2xl font-medium lga:text-4xl">ASCEE Partners</h3>
         <div class="mt-4 flex flex-col">
             <div class="flex flex-col items-center lga:flex-row lga:items-start">
-                <a href="https://www.uii.ac.id/"> <img class="lga:h-auto xla:h-28" src='assets/partner1.png'
-                        alt=""></a>
-                <a href="https://www.crossref.org/"> <img class="lga:h-auto xla:h-28" src='assets/partner2.png'
-                        alt=""></a>
-                <a href="https://kemlu.go.id/beijing/en"> <img class="lga:h-auto xla:h-28" src='assets/partner3.png'
-                        alt=""></a>
-                <a href="https://app.grammarly.com/"> <img class="lga:h-auto xla:h-28" src='assets/partner4.png'
-                        alt=""></a>
-                <a href="https://oversea.cnki.net/index/"> <img class="lga:h-auto xla:h-28" src='assets/partner5.png'
-                        alt=""></a>
+                <a href="https://www.uii.ac.id/"> <img class="lga:h-auto xla:h-28"
+                        src='build/assets/images/partner1.png' alt=""></a>
+                <a href="https://www.crossref.org/"> <img class="lga:h-auto xla:h-28"
+                        src='build/assets/images/partner2.png' alt=""></a>
+                <a href="https://kemlu.go.id/beijing/en"> <img class="lga:h-auto xla:h-28"
+                        src='build/assets/images/partner3.png' alt=""></a>
+                <a href="https://app.grammarly.com/"> <img class="lga:h-auto xla:h-28"
+                        src='build/assets/images/partner4.png' alt=""></a>
+                <a href="https://oversea.cnki.net/index/"> <img class="lga:h-auto xla:h-28"
+                        src='build/assets/images/partner5.png' alt=""></a>
             </div>
             <div class="flex flex-col items-center lga:flex-row lga:items-start">
 
                 <a href="https://www.dstu.dp.ua/uni/index.html"> <img class="lga:h-auto xla:h-28"
-                        src='assets/partner6.png' alt=""></a>
-                <a href="https://www.google.com/"> <img class="lga:h-auto xla:h-28" src='assets/partner7.png'
-                        alt=""></a>
+                        src='build/assets/images/partner6.png' alt=""></a>
+                <a href="https://www.google.com/"> <img class="lga:h-auto xla:h-28"
+                        src='build/assets/images/partner7.png' alt=""></a>
                 <a href="https://www.unibarranquilla.edu.co/"> <img class="lga:h-auto xla:h-28"
-                        src='assets/partner8.png' alt=""></a>
-                <a href="https://untad.ac.id/en/"> <img class="lga:h-auto xla:h-28" src='assets/partner9.png'
-                        alt=""></a>
-                <a href="https://isi-ska.ac.id/"> <img class="lga:h-auto xla:h-28" src='assets/partner10.png'
-                        alt=""></a>
+                        src='build/assets/images/partner8.png' alt=""></a>
+                <a href="https://untad.ac.id/en/"> <img class="lga:h-auto xla:h-28"
+                        src='build/assets/images/partner9.png' alt=""></a>
+                <a href="https://isi-ska.ac.id/"> <img class="lga:h-auto xla:h-28"
+                        src='build/assets/images/partner10.png' alt=""></a>
             </div>
         </div>
         <div class="mt-12 flex flex-col gap-12 xla:flex-row">
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-    base: '',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+        }),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: 'resources/images/*',
+                    dest: 'assets/images',
+                },
+            ],
         }),
     ],
 });
